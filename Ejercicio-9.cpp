4615e28362b82d015ef6acc101cea5cf38f8c02a
Samuel-Q <quesadasamuel72@gmail.com>
2025-09-17 18:17:01 -0500

    Definimos las variables y le pedimos al usuario los datos para el programa

---
+++ Ejercicio-9.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+int main() {
+    int a;
+    int b;
+    int c=0;
+    int d=0;
+    std::cout<<"////// Máximo Común Divisor //////"<<std::endl;
+    std::cout<<"Ingresa el primer número: ";
+    std::cin>>a;
+    std::cout<<"Ingresa el segundo número: ";
+    std::cin>>b;
+    int at=a;
+    int bt=b;
+}
