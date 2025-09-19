3a3ec6336c2ccfff5594e33438c8f39359183f68
Samuel-Q <quesadasamuel72@gmail.com>
2025-09-18 23:57:41 -0500

    Mejoramos la interfaz y mostramos los resultados.

--- Ejercicio-2_2.cpp
+++ Ejercicio-2_2.cpp
@@ -7,9 +7,13 @@ int main(){
     int i=0;
     int id[10]={245,645,867,123,564,768,987,555,324,115};
     float dist[10];
-    float suma;
+    float suma=0;
     int j=1;
+    int con;
 
+    cout<<"================================"<<endl;
+    cout<<"'''''' REGISTRO DISTANCIA ''''''"<<endl;
+    cout<<"================================\n"<<endl;
     while(i<10){
         cout<<"El conductor con el ID: "<<id[i]<<endl;
         cout<<"por favor ingrese su distancia recorrida(Km): ";
@@ -30,9 +34,17 @@ int main(){
     while(j<10){
         if(dist[j]>a){
             a=dist[j];
-            int con=j
+            con=j;
         }   
         j++;
     }    
+    cout<<"\tLa distancia promedio recorrida por los conductores fue de: "<<prom<<"Km"<<endl;
+    cout<<"\tEl conductor con el ID "<<id[con]<<" obtuvo la mayor distancia de recorrido con: "<<dist[con]<<"km"<<endl;
+    cout<<"\tLa distancia total recorrida por los conductores fue de: "<<suma<<"Km"<<endl;
+    cout<<"\t\tGRACIAS (nos esforzamos mucho)"<<endl;
+    
+
+
+
 
 }
