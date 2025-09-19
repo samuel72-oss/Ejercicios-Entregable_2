23f6cbe32e3aa69d59644322c87e6614ca1756f1
Samuel-Q <quesadasamuel72@gmail.com>
2025-09-18 23:19:29 -0500

    Pedimos a lños usuarios ingresar su distancia recorrida de acuerdo a su numero de ID.

--- Ejercicio-2_2.cpp
+++ Ejercicio-2_2.cpp
@@ -8,5 +8,14 @@ int main(){
     int id[10]={245,645,867,123,564,768,987,555,324,115};
     float dist[10];
 
+    while(i<10){
+        cout<<"El conductor con el ID: "<<id[i]<<endl;
+        cout<<"por favor ingrese su distancia recorrida(Km): ";
+        cin>>dist[i];
+        cout<<"Gracias por tu labor"<<endl;
+        cout<<"\n";
+        i++;
+
+    }
 
 }
