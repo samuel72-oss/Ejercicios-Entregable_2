d505f76ffce891a4685fa7d65e8a4cbb8fc2697c
Samuel-Q <quesadasamuel72@gmail.com>
2025-10-16 22:58:38 -0500

    Le pedimos al usuario ingresar la cantidad de dias que conoce el mercado, creamos un condicional para no permitir que sean mas de 7 dias y mediante un ciclo rellenamos el vector.

--- 2entrega/121_Best-Time-to-Buy-and-Sell-Stock.cpp
+++ 2entrega/121_Best-Time-to-Buy-and-Sell-Stock.cpp
@@ -15,4 +15,24 @@ int main(){
     cout<<"\t===================================="<<endl;
     cout<<"\tBIENVENID@ AL PROGRAMA DE GANANCIAS"<<endl;
     cout<<"\t====================================\n"<<endl;
+    cout<<"Para poder analizar las ganancias del mercado y exponerte los resultados ingresa cuantos dias conoces el precio esta semana: ";
+    cin>>n;
+    while(a==false){
+        if(n<8){
+            a=true;
+        }
+        else{
+            cout<<"Es imposible, la semana maximo tiene 7 dias\n"<<endl;
+            cout<<"Vuelve a ingresar: ";
+            cin>>n;
+        }
+        
+    }
+    cout<<"\n";
+    int ss[n];
+    while(i<n){
+        cout<<"Ingresa el valor de mercado del dia "<<i+1<<": ";
+        cin>>ss[i];
+        i++;
+    }
 }
