3281e997a91395d24443b2dfe64e70965053dc5e
Samuel-Q <quesadasamuel72@gmail.com>
2025-10-16 23:21:29 -0500

    Mediante un ciclo dentro de un ciclo creamos el triangulo de pascal con dos contadores i y j.

--- 2entrega/118_Pascals-Triangle.cpp
+++ 2entrega/118_Pascals-Triangle.cpp
@@ -7,4 +7,16 @@ int main() {
     cout<<"Ingrese el número de filas para el triángulo de Pascal: ";
     cin>>fil;
     int pascal[fil][fil];
+    while (i < fil) {
+        int j = 0;
+        while (j <= i) {
+            if (j == 0 || j == i)
+                pascal[i][j] = 1;
+            else
+                pascal[i][j] = pascal[i - 1][j - 1] + pascal[i - 1][j];
+            j++;
+        }
+        i++;
+    }
+
 }
